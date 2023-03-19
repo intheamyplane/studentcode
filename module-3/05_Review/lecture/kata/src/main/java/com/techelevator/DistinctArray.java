182f13347d4b79a478cab1fff2b968c4ba57821d
Amy Edwards <amy.m.edwards1990@gmail.com>
2023-03-18 20:45:35 -0400

    hw submission

--- module-3/05_Review/lecture/kata/src/main/java/com/techelevator/DistinctArray.java
+++ module-3/05_Review/lecture/kata/src/main/java/com/techelevator/DistinctArray.java
@@ -1,40 +1,73 @@
 package com.techelevator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class DistinctArray {
     /**
      * You are given an input array of integers. Write a function to remove any duplicate
      * numbers from the input array
-     *
-     *  for example, given array:
-     *
-     *  [2, 3, 1, 8, 4, 8, 5, 1, 9, 8, 6]
-     *
-     *  the function returns:
-     *
-     *  [2, 3, 1, 8, 4, 5, 9, 6]
-     *
-     *  Provide 2 implementations for this function:
-     *      A) - The unique numbers in the returned array can be in ANY order
-     *      (easier case).
-     *      Implement inside removeDuplicates() function below.
-     *
-     *      B) - The unique numbers in the returned array must be in the SAME
-     *      order as the input array (see example above).
-     *      Implement inside removeDuplicatesPreserveOrder() function below.
-     *
-     *      NOTE: This kata is similar to a problem that you may get as a part of an interview assessment.
-     *      To keep this exercise realistic, PLEASE DON'T SEARCH THE WEB FOR A COMPLETE SOLUTION TO
-     *      REMOVING DUPLICATES FROM AN ARRAY.
-     *
+     * <p>
+     * for example, given array:
+     * <p>
+     * [2, 3, 1, 8, 4, 8, 5, 1, 9, 8, 6]
+     * <p>
+     * the function returns:
+     * <p>
+     * [2, 3, 1, 8, 4, 5, 9, 6]
+     * <p>
+     * Provide 2 implementations for this function:
+     * A) - The unique numbers in the returned array can be in ANY order
+     * (easier case).
+     * Implement inside removeDuplicates() function below.
+     * <p>
+     * B) - The unique numbers in the returned array must be in the SAME
+     * order as the input array (see example above).
+     * Implement inside removeDuplicatesPreserveOrder() function below.
+     * <p>
+     * NOTE: This kata is similar to a problem that you may get as a part of an interview assessment.
+     * To keep this exercise realistic, PLEASE DON'T SEARCH THE WEB FOR A COMPLETE SOLUTION TO
+     * REMOVING DUPLICATES FROM AN ARRAY.
      */
 
     public int[] removeDuplicates(int[] input) {
 
-        throw new UnsupportedOperationException("This method is not yet implemented");
-    }
+        List<Integer> broccoli = new ArrayList<>();
+        List<Integer> carrot = new ArrayList<>();
 
-    public int[] removeDuplicatesPreserveOrder(int[] input) {
+            if(input==null){
+                throw new NullPointerException("null");
+            }
+            else {
+                for (int i = 0; i < input.length; i++) {
 
-        throw new UnsupportedOperationException("This method is not yet implemented");
-    }
-}
+                    broccoli.add(input[i]);
+                }
+
+                for (int i = 0; i < input.length; i++) {
+                    if (carrot.contains(broccoli.indexOf(i))) {
+                        continue;
+                    } else {
+                        carrot.add(broccoli.indexOf(i));
+                    }
+                }
+                int[] result = new int[carrot.size()];
+                for (int i : carrot) {
+                    result[i] = carrot.indexOf(i);
+
+                }
+            }
+
+            return result;}
+
+
+
+
+
+            public int[] removeDuplicatesPreserveOrder (int[] input){
+
+                throw new UnsupportedOperationException("This method is not yet implemented");
+            }
+        }
+
+
